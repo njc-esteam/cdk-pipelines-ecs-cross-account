ed8e4616898e59d402e9e3ac1dd673460043b8ea
Nicholas Clay <nick@fourfire.com>
2022-12-11 10:08:34 -0500

    Initial

---
+++ bin/cdk-pipeline.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
+import { Params } from '../lib/params';
+
+const app = new cdk.App();
+new CdkPipelineStack(app, 'CdkPipelineStack', {
+  env: {
+    account: Params.DEV_ACCOUNT_ID,
+    region: Params.AWS_REGION_PROD
+  }
+});
---
+++ lib/params.ts
@@ -0,0 +1,10 @@
+export const Params = {
+    GITHUB_REPO: "njc-esteam/es-test-pipeline",
+    BRANCH_NAME: "main",
+    GITHUB_TOKEN: "GitHubToken",
+    AWS_REGION_STAGING: "us-west-2",
+    AWS_REGION_PROD: "us-west-2",
+    DEV_ACCOUNT_ID: "284404439619",
+    STAGING_ACCOUNT_ID: "252186317607",
+    PROD_ACCOUNT_ID: "252186317607"
+} as const
